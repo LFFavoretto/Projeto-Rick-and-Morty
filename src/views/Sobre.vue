fcf5474805bd4b4b2c9a548f4a08bd161d1cec46
lianccn <lianmarinheiro2005@gmail.com>
2026-06-01 21:33:09 -0300

    adicionando Sobre.vue com a descrição do projeto

---
+++ src/views/Sobre.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container py-5">
+        <div>
+            <h1>Sobre o Projeto</h1>
+            <p>Este é um projeto desenvolvido para demonstrar o uso de Vue.js e a API do Rick and Morty.</p>
+            <p>O objetivo é praticar os conceitos de componentes, rotas, e consumo de API e estilização com Bootstrap. </p>
+            <p>
+                Tecnologias utilizadas: 
+                <span class="badge bg-primary">Vue.js 3</span> 
+                <span class="badge bg-primary">Vue Router</span> 
+                <span class="badge bg-primary">Bootstrap 5</span>
+                <span class="badge bg-primary">JavaScript</span>
+            </p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
